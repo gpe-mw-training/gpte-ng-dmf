ecaf50aaf21089113f67b7539a0d049bfe87bde1
diego-torres <dtorresf@redhat.com>
2018-02-07 16:42:54 -0600

    Running on port 8080 and starting for ocp start

---
+++ server.js
@@ -0,0 +1,31 @@
+// Get dependencies
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Catch all routes and return the index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+/**
+ * Get port from environment and store in Express.
+ */
+const port = process.env.PORT || '8080';
+app.set('port', port);
+
+/**
+ * Create HTTP server.
+ */
+const server = http.createServer(app);
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+server.listen(port, () => console.log(`API running on localhost:${port}`));
