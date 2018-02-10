35b3c303d9534e59aeb4dc6b5bcd43f05c18abd2
diego-torres <dtorresf@redhat.com>
2018-02-10 15:35:32 -0600

    remove unnecesary dependencies

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { NgxEchartsModule } from 'ngx-echarts';
 
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
@@ -9,7 +8,6 @@ import { StatusCardComponent } from './status-card/status-card.component';
 @NgModule({
   imports: [
     ThemeModule,
-    NgxEchartsModule,
   ],
   declarations: [
     DashboardComponent,
