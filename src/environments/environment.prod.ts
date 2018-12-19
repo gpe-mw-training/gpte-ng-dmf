dd1b14d9bdddc2351b1c94896b5ba8f39819f5be
diego-torres <dtorresf@redhat.com>
2018-12-18 19:57:34 -0600

    fix ejs as prod dependency

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright Akveo. All Rights Reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ */
+export const environment = {
+  production: true,
+  dmApiUrl: 'http://demo-kieserver-dm-dtf.apps.dev37.openshift.opentlc.com/',
+  dmUserName: 'adminUser',
+  dmCredentials: 'test1234!',
+};
